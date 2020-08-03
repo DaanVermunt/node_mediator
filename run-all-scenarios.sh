c5f8296aba07e334ff56f4938447098e58a7e927
Daan Vermunt <daan@protium.digital>
2020-08-03 20:41:17 +0200

    add runs

--- run-all-scenarios.sh
+++ run-all-scenarios.sh
@@ -1,7 +1,18 @@
 #!/usr/bin/env bash
 
+function max8 {
+   while [ `jobs | wc -l` -ge 8 ]
+   do
+      sleep 5
+   done
+}
+
 for f in ./data/scenarios/*.json; do
     f1="${f##*/}"
-    ts-node ./src/main.ts -i "$f" -o "${f1%.json}" -s mdp &
+    for i in {1..10}; do
+      max8; ts-node ./src/main.ts -i "$f" -o "${f1%.json}" -s mdp -n $i &
+      max8; ts-node ./src/main.ts -i "$f" -o "${f1%.json}" -s heuristic -n $i &
+    done
 #    ts-node ./src/main.ts -i "$f" -o "${f1%.json}" -s passive &
 done
+wait
